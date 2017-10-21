9b4f4558de4dad6a8ae975309fe2e2f27a991883
T P MAREMANE <50689924@mylife.unisa.ac.za>
2017-10-21 22:23:35 +0200

    First Commit

---
+++ XamarinRestClient/XamarinRestClient/NetworkState.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinRestClient
+{
+	public class NetworkState
+	{
+
+		public static bool IsInternet()
+		{
+			if (CrossConnectivity.Current.IsConnected)
+			{
+				return true;
+			}
+			else
+			{
+				     
+				return false;
+			}
+		}
+
+	}
+}
